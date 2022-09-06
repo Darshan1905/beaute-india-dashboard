9d1ed012548b65c75bf2eeea0016b126b1417a85
Darshan1905 <darshanprajapati1905@gmail.com>
2022-09-06 14:22:51 +0530

    shop controller changes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PermissionController;
-use App\Http\Controllers\shopController;
+use App\Http\Controllers\ShopController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\VendorController;
 use App\Http\Controllers\ProductController;
@@ -37,8 +37,8 @@ Route::group(['middleware' => ['auth']], function() {
     Route::resource('roles', RoleController::class);
     Route::resource('permissions', PermissionController::class);
     
-    Route::resource('shop', shopController::class);
-    Route::get('shop-List/', [shopController::class, 'shopList'])->name('shops-list');
+    Route::resource('shop', ShopController::class);
+    Route::get('shop-List/', [ShopController::class, 'shopList'])->name('shops-list');
     
     Route::resource('categorys', CategoryController::class);
     Route::get('category-list/', [CategoryController::class, 'categoryList'])->name('category-list');
