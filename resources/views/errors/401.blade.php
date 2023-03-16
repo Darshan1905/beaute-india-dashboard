60a8f74db5ea736ff6505fef96ffff8736174902
Darshan Prajapati <darshanprajapati@Darshans-MacBook-Air.local>
2023-03-16 16:45:20 +0530

    message changed

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
-@section('title', __('Unauthorized'))
+@section('title', __('Login first'))
 @section('code', '401')
-@section('message', __('Unauthorized'))
+@section('message', __('Login first'))
